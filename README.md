e990b52154f42e82f1a28af03bf7324a9da8d491
rafaellapuente <82778110+rafaellapuente@users.noreply.github.com>
2022-04-01 14:01:01 -0300

    ATT

--- README.md
+++ README.md
@@ -4,9 +4,13 @@ Projeto em React + Typescript fazendo uma calculadora de IMC.
 
 Projeto desenvolvido durante o curso [B7Web](https://b7web.com.br/).
 
+### Programas necessários para rodar jogos
+
+- `NodeJS v16.14.2`
+
 ### Instaçação
 
-- `npm start`
+- `npm install`
 
 ### Para rodar
 
--- src/helpers/imc.ts
+++ src/helpers/imc.ts
@@ -1,5 +1,3 @@
-import { resourceUsage } from "process";
-
 export type Level = {
   title: string;
   color: string;
@@ -9,8 +7,8 @@ export type Level = {
 };
 
 export const levels: Level[] = [
-  { title: "Magreza", color: "#96A3AB", icon: "down", imc: [0, 18.5] },
-  { title: "Normal", color: "#0EAD69", icon: "up", imc: [18.6, 24.9] },
+  { title: "Magreza", color: "#96A3AB", icon: "down", imc: [0, 18.59] },
+  { title: "Normal", color: "#0EAD69", icon: "up", imc: [18.6, 24.99] },
   { title: "Sobrepeso", color: "#E2B039", icon: "down", imc: [25, 30] },
   { title: "Obesidade", color: "#C3423F", icon: "down", imc: [30.1, 99] },
 ];
